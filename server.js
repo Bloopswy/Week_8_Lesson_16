bbe851166c7f27598a2411acdc530763839e2498
Bloopswy <Bernicelwy2005@gmail.com>
2026-01-24 01:51:53 +0800

    updated

--- server.js
+++ server.js
@@ -45,10 +45,10 @@ app.post('/addmovie', async (req,res) => {
 });
 
 app.delete('/deletemovie/:id', async (req,res) => {
-    const {id} = req.body;
+    const {id} = req.params;
     try {
         let connection = await mysql.createConnection(dbConfig);
-        await connection.execute('DELETE FROM movies WHERE id ='+ [id]);
+        await connection.execute('DELETE FROM movies WHERE id ='+ id);
         res.json({message: 'Movie deleted successfully'});
     }catch(err) {
         console.error(err);
@@ -57,6 +57,7 @@ app.delete('/deletemovie/:id', async (req,res) => {
 });
 
 app.put('/updatemovie', async (req,res) => {
+    const {id} =req.params;
     const {movie_name, movie_year, movie_pic, id} = req.body;
     try {
         let connection = await mysql.createConnection(dbConfig);
